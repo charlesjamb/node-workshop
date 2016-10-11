8c14117e271804d0e400d744f5628879c38cd114
charlesjamb <charles.jambon@gmail.com>
2016-10-11 14:09:19 -0400

    finished guessing game

--- number-guessing-game.js
+++ number-guessing-game.js
@@ -1,32 +1,43 @@
 var prompt = require('prompt');
-var alert = require('alerts');
-
-alert('Foo');
 
 var randomNum = Math.floor(Math.random()*100);
 var list = [];
+var count = 0;
 
-prompt.get('Guess a number between 1 and 100', function(err, input) {
-	if (err) {
-		console.log('Oops there was an error')
-	}
-	else {
-		for (var i = 0; i < 4; i++) {
-			var userInput = parseInt(input);
-			if (userInput === randomNum) {
-				alert('savage');
-			}
-			else if (userInput < randomNum && i != 3) {
-				alert('try higher');
-			}
-			else if (userInput > randomNum && i != 3) {
-				alert('try lower');
-			}
-			list.push(userInput);
+function demander() {
+	prompt.get('Guess a number between 1 and 100', function(err, input) {
+		if (err) {
+			console.log('Oops there was an error')
 		}
-		alert('The number was ' + randomNum + '\n' + 'You tried ' + list);
+		else {
+			var userInput = parseInt(input['Guess a number between 1 and 100']);
+
+			interpreter(userInput);
+		}
+	})
+
+}
+
+function interpreter(input) {
+	list.push(input);
+	count += 1;
+	if (input === randomNum) {
+		console.log('You guessed the right number');
+		count = 4;
 	}
-})
+	else if (input < randomNum && count != 4) {
+		console.log('try higher');
+	}
+	else if (input > randomNum && count != 4) {
+		console.log('try lower');
+	}
+	else if (count = 4) {
+		console.log('The number was ' + randomNum + '\n' + 'You tried ' + list);
+	}
 
+	if (count < 4) {
+		demander();
+	}
+}
 
-
+demander();
